abe1a6e5f14541da2a6fea5414e5d7e73c0b44af
Joni <jonivalles@gmail.com>
2018-06-02 13:57:13 +0200

    Prueba

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import json 
 import requests
+import time
 
 TOKEN = "269022555:AAFOpX8kGgLbgYK5Gyh_ek2gpfj6WJWxIpw"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
@@ -17,10 +18,18 @@ def get_json_from_url(url):
     return js
 
 
-def get_updates():
-    url = URL + "getUpdates"
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
     js = get_json_from_url(url)
     return js
+	
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
 
 
 def get_last_chat_id_and_text(updates):
@@ -29,6 +38,15 @@ def get_last_chat_id_and_text(updates):
     text = updates["result"][last_update]["message"]["text"]
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
     return (text, chat_id)
+	
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
 
 
 def send_message(text, chat_id):
@@ -36,5 +54,15 @@ def send_message(text, chat_id):
     get_url(url)
     
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+def main():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
